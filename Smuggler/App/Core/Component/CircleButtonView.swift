43aee693aa4744c902f42dc710f59191fa62be8f
Oleg <oleg@mkpwnz.moe>
2022-05-29 22:22:38 +0200

    Create ButtonCircleView and SerachBarView

---
+++ Smuggler/App/Core/Component/CircleButtonView.swift
@@ -0,0 +1,31 @@
+//
+//  CircleButtonView.swift
+//  Smuggler
+//
+//  Created by mk.pwnz on 29/05/2022.
+//
+
+import SwiftUI
+
+struct CircleButtonView: View {
+    let iconName: String
+    var body: some View {
+        Image(systemName: iconName)
+            .font(.headline)
+            .foregroundColor(.theme.accent)
+            .frame(width: 50, height: 50)
+            .background(
+                Circle()
+                    .foregroundColor(.theme.background)
+            )
+            .shadow(color: .theme.accent.opacity(0.25),
+                    radius: 10, x: 0, y: 0)
+            .padding()
+    }
+}
+
+struct CircleButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+    CircleButtonView(iconName: "heart.fill")
+    }
+}
---
+++ Smuggler/App/Core/Component/SearchBarView.swift
@@ -0,0 +1,59 @@
+//
+//  SearchBarView.swift
+//  Smuggler
+//
+//  Created by mk.pwnz on 29/05/2022.
+//
+
+import SwiftUI
+
+
+struct SearchBarView: View {
+    @Binding var searchText: String
+    
+    var body: some View {
+        HStack {
+            Image(systemName: "magnifyingglass")
+                .foregroundColor(searchText.isEmpty ? .theme.secondaryText : .theme.accent)
+            
+            TextField("Type something...", text: $searchText)
+                .foregroundColor(.theme.accent)
+                .disableAutocorrection(true)
+                .overlay(
+                    Image(systemName: "xmark.circle.fill")
+                        .padding()
+                        .offset(x: 10)
+                        .foregroundColor(.theme.accent)
+                        .opacity(searchText.isEmpty ? 0 : 1)
+                        .onTapGesture {
+                            UIApplication.shared.endEditing()
+                            searchText = ""
+                        },
+                    alignment: .trailing
+                )
+        }
+        .font(.headline)
+        .padding()
+        .background(RoundedRectangle(cornerRadius: 25)
+            .fill(Color.theme.background)
+            .shadow(
+                color: .theme.accent.opacity(0.25),
+                radius: 10, x: 0.0, y: 0.0)
+        )
+        .padding()
+    }
+}
+
+struct SearchBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            SearchBarView(searchText: .constant(""))
+                .previewLayout(.sizeThatFits)
+                .preferredColorScheme(.dark)
+            
+            SearchBarView(searchText: .constant(""))
+                .previewLayout(.sizeThatFits)
+            
+        }
+    }
+}
